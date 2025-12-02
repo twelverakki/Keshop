090b0553813b5ec329c3d9da7766eb338c4df203
twelverakki <twelve.rakki@gmail.com>
2025-12-02 23:55:11 +0800

    kelewat dikit

--- resources/views/dashboard/seller/products/index.blade.php
+++ resources/views/dashboard/seller/products/index.blade.php
@@ -74,7 +74,7 @@
                                                 style="display: none;">
 
                                                 <div class="py-1">
-                                                    <a href="{{ route('seller.products.edit', $product->id) }}" class="text-gray-700 hover:bg-gray-100 block px-4 py-2 text-sm flex items-center gap-2">
+                                                    <a href="{{ route('seller.products.edit', $product->id) }}" class="text-gray-700 hover:bg-gray-100 px-4 py-2 text-sm flex items-center gap-2">
                                                         <svg class="w-4 h-4 text-blue-500" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>
                                                         Edit Product
                                                     </a>
